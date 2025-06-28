369cd3a21408fce14fd173dba4279356ff069354
alantr9 <trinh.alan2020@gmail.com>
2025-06-28 00:42:49 -0700

    Intial Tokenizer - tokentypes and individual tokens

--- Personal Database/Tokenizer.h
+++ Personal Database/Tokenizer.h
@@ -2,8 +2,31 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <vector>
 
+enum class tokenType
+{
+    keyword,
+    identifier,
+    symbol,
+    stringLiteral,
+    numberLiteral,
+    endOfFile,
+    Unknown
+};
+
+
+struct Token {
+    tokenType   type;
+    std::string text;
+    size_t      position; // position in token
+
+    Token(tokenType t, std::string val, size_t pos)
+        : type(t), text(std::move(val)), position(pos) {
+    }
+};
 
 
 #endif 
