91a2af5fc8011274c2c9dd6cea69ecf4c0f39c70
alantr9 <trinh.alan2020@gmail.com>
2025-07-04 14:43:11 -0700

    Added function that parses indentifiers and keywords in the query

--- Personal Database/Tokenizer.cpp
+++ Personal Database/Tokenizer.cpp
@@ -17,3 +17,25 @@ bool tokenizer::hasMoreTokens() const
 	return position < m_input.size();
 }
 
+token tokenizer::readIdentifierOrKeyword()
+{
+	size_t start = position;
+	// Reads one word in the the query
+	while(position < m_input.size() && (std::isalnum(m_input[position]) || m_input[position] == '_')) 
+	{
+		++position;
+	}	
+
+	std::string queryWord = m_input.substr(start, position - start);
+	if (queryWord == "CREATE" || queryWord == "INSERT" || queryWord == "SELECT" ||
+		queryWord == "DELETE" || queryWord == "DROP" || queryWord == "TABLE" || 
+		queryWord == "INDEX") 
+	{
+		return token(tokenType::keyword, queryWord, start);
+	} 
+	else 
+	{
+		return token(tokenType::identifier, queryWord, start);
+	}
+}
+
